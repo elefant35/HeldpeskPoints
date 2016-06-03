e904a235baa04e4b8b1bf51c0dba952df121b998
elefant35 <tyler.coleman@gmail.com>
2016-06-03 14:12:07 -0700

    Added functionality to receive messages and post in response

--- hdp.php
+++ hdp.php
@@ -1,5 +1,38 @@
 <?php
+//Globals-----------------------------
+$Wurl="https://hooks.slack.com/services/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";
 
+//functions---------------------------
+function sendPackage($data, $Wurl){
+        $ch = curl_init($Wurl);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+        curl_exec($ch);
+        curl_close($ch);
+
+
+}
+function makePackage($string)
+{
+        $data= "payload=" . json_encode(array(
+//        "channel" => "#{$_POST['channel_name']}",
+        "text" => $string));
+
+
+        return $data;
+}
+//main------------------------------------
+$token = $_POST['token'];
+if($token !='KvEAVM3BF3ox2SRfjHuUw0J8'){
+        echo ('<img src= http://vignette1.wikia.nocookie.net/muppet/images/5/5b/Oscar-can.png/revision/latest?cb=20120117061845>');
+        die("Go Away");
+}
+
+echo("test");
+$text = makePackage("not hello");
+sendPackage($text, $Wurl);
+
+
+echo("$text");
 
 
 ?>
